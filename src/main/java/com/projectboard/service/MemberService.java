6c62225d0c9519e9f7ce28ebb5d7522b684e32a9
gogowonji <spacedau@gmail.com>
2024-07-15 16:04:59 +0900

    필요 없는 import 삭제

--- src/main/java/com/projectboard/service/MemberService.java
+++ src/main/java/com/projectboard/service/MemberService.java
@@ -6,8 +6,6 @@ import com.projectboard.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
